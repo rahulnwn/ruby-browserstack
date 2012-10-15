ddf2b7fc6cfbdbfd79a6d8ca30f2b83a610ab78c
Rahul Nawani <rahulnwn@gmail.com>
2012-10-15 06:22:10 +0530

    removed create client method

--- lib/browserstack.rb
+++ lib/browserstack.rb
@@ -3,11 +3,3 @@ require "net/http"
 require "yajl"
 require "browserstack/client"
 require "browserstack/version"
-
-module Browserstack
-  def self.create_client(params)
-    client = Client.new(params)
-    puts "Client created"
-    client
-  end
-end
